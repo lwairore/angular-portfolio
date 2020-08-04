ffcde7fa351f0afab1a2824db3550de7f9dc6eb8
lwairore <kwairore@gmail.com>
2020-08-05 01:01:03 +0300

    - Add children components to HomepageComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PortfolioCardsComponent } from './homepage/portfolio-cards/portfolio-cards.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomepageComponent
+    path: '', component: HomepageComponent, children: [
+      {
+        path: '',
+        component: PortfolioCardsComponent
+      }
+    ]
   },
 ];
 
