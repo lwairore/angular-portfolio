1daa6e0edf4756bc96abbafa2a0453b887fbaa15
lwairore <kwairore@gmail.com>
2020-08-05 00:00:11 +0300

    - Declare ContactMeComponent.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, ContactMeComponent],
   imports: [
     CommonModule
   ],
