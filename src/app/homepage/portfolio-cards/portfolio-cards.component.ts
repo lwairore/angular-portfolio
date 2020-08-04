3995b00596b2bca7f81927eeb0e9174a6d633092
lwairore <kwairore@gmail.com>
2020-08-05 00:56:51 +0300

    - Generate PortfolioCardsComponent.

---
+++ src/app/homepage/portfolio-cards/portfolio-cards.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-portfolio-cards',
+  templateUrl: './portfolio-cards.component.html',
+  styles: [
+  ]
+})
+export class PortfolioCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
