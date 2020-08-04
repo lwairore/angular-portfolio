be7dc01e3b11024eed7799218c7c2d642f60f65a
lwairore <kwairore@gmail.com>
2020-08-05 00:40:52 +0300

    - Declare PortfolioGridComponent.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { PageTitleComponent } from './page-title/page-title.component';
+import { PortfolioGridComponent } from './portfolio-grid/portfolio-grid.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent, PageTitleComponent],
+  declarations: [HomepageComponent, PageTitleComponent, PortfolioGridComponent],
   imports: [
     CommonModule
   ]
