b499c5691d2217f18e7f78a6a4f665367078a7b0
lwairore <kwairore@gmail.com>
2020-08-05 00:40:31 +0300

    - Generate PortfolioGridComponent.

---
+++ src/app/homepage/portfolio-grid/portfolio-grid.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-portfolio-grid',
+  templateUrl: './portfolio-grid.component.html',
+  styles: [
+  ]
+})
+export class PortfolioGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
