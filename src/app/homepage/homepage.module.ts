6f667c6b53f4eba9b6c7ff65292a0e65df1b601d
lwairore <kwairore@gmail.com>
2020-08-04 23:39:39 +0300

    - Properly fix the arragment of components.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
-import { PageTitleComponent } from '../page-title/page-title.component';
+import { PageTitleComponent } from './page-title/page-title.component';
 
 
 
