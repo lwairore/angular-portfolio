f9c3b7fce50a1e812ff1b2d6eefece024683185f
lwairore <kwairore@gmail.com>
2020-08-04 23:36:44 +0300

    - Generate PageTitleComponent.

---
+++ src/app/page-title/page-title.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-page-title',
+  templateUrl: './page-title.component.html',
+  styles: [
+  ]
+})
+export class PageTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
