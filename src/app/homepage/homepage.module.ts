746362bd796362ac44d428e7411f5f3e82604002
lwairore <kwairore@gmail.com>
2020-08-04 23:37:13 +0300

    - Declare PageTitleComponent.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
+import { PageTitleComponent } from '../page-title/page-title.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, PageTitleComponent],
   imports: [
     CommonModule
   ]
