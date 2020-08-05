4e6d27243eb31f9eedf29a67a221b5832073c035
lwairore <kwairore@gmail.com>
2020-08-05 14:13:30 +0300

    - Store dummy projects data.

---
+++ src/app/core/mock-project.ts
@@ -0,0 +1,88 @@
+import { Project } from './project';
+
+export const PROJECTS: Project[] = [
+    {
+        heading: `Pitch`,
+        slug: `pitch`,
+        tags: [
+            {
+                title: `Flask`,
+                slug: `flask`
+            }
+        ],
+        portfolioThumb: '/assets/img/portfolio/pitch.png'
+    },
+    {
+        heading: `InstaClone`,
+        slug: `instaclone`,
+        tags: [
+            {
+                title: `Django`,
+                slug: `django`
+            }
+        ],
+        portfolioThumb: `/assets/img/portfolio/instaclone.png`
+    },
+    {
+        heading: `News-Highlights`,
+        slug: `news-highlights`,
+        tags: [
+            {
+                title: `Flask`,
+                slug: `flask`
+            }
+        ],
+        portfolioThumb: `/assets/img/portfolio/news-highlights.png`
+    },
+    {
+        heading: `Pig-Dice`,
+        slug: `pig-dice`,
+        tags: [
+            {
+                title: `Javascript`,
+                slug: `javascript`
+            },
+            {
+                title: `HTML`,
+                slug: `hmtl`
+            },
+            {
+                title: `CSS`,
+                slug: `css`
+            },
+            {
+                title: `Bootstrap`,
+                slug: `bootstrap`
+            }],
+        portfolioThumb: `/assets/img/portfolio/pig-dice.png`
+    },
+    {
+        heading: `Quotes`,
+        slug: `quotes`,
+        tags: [
+            {
+                title: `Angular`,
+                slug: `angular`
+            }
+        ],
+        portfolioThumb: `/assets/img/portfolio/quotes.png`
+    },
+    {
+        heading: `Quiz-Board`,
+        slug: `quiz-board`,
+        tags: [
+            {
+                title: `JavaScript`,
+                slug: `javascript`
+            },
+            {
+                title: `HTML`,
+                slug: `html`
+            },
+            {
+                title: `CSS`,
+                slug: `css`
+            }],
+        portfolioThumb: `/assets/img/portfolio/quiz-board.png`
+    }
+]
