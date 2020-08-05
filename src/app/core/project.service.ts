0449586692511059994f329e87a43b5ae110b8d2
lwairore <kwairore@gmail.com>
2020-08-05 13:02:42 +0300

    - Register ProjectService at the CoreModule level. - Add getAllProjects() method that returns projects$ object. - Add getProject() method that returns a specific project given the projectSlug value.

--- src/app/core/project.service.ts
+++ src/app/core/project.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { CoreModule } from './core.module';
+import { Project } from './project';
+import { BehaviorSubject } from 'rxjs';
+import { PROJECTS } from './mock-project';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: CoreModule
 })
 export class ProjectService {
+  private projects$: BehaviorSubject<Project[]>;
 
-  constructor() { }
+  constructor() { 
+    this.projects$ = new BehaviorSubject<Project[]>(PROJECTS);
+  }
+
+  getAllProjects(): BehaviorSubject<Project[]> {
+    return this.projects$;
+  }
+
+  getProject(projectSlug: string) {
+    return this.getAllProjects().pipe(
+      map(projects => projects.find(project => project.slug === projectSlug))
+    );
+  }
 }
