2a002e4ac6a5d09fcb945231f197b377501432a8
lwairore <kwairore@gmail.com>
2020-08-05 21:28:19 +0300

    - Generate ProjectDescriptionComponent.

---
+++ src/app/project-detail/project-description/project-description.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-project-description',
+  templateUrl: './project-description.component.html',
+  styles: [
+  ]
+})
+export class ProjectDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
