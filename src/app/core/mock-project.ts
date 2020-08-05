9119111314a57b4b67bce5de252b3789674f9ab7
lwairore <kwairore@gmail.com>
2020-08-05 22:41:36 +0300

    - Add missing collaborators property.

--- src/app/core/mock-project.ts
+++ src/app/core/mock-project.ts
@@ -17,7 +17,15 @@ export const PROJECTS: Project[] = [
         createdAt: new Date(`Aug 28, 2020`),
         projectGallery: `/assets/img/portfolio/single/slider/01.jpg`,
         githubRepo: `https://github.com/{:owner}/{:repoName}`,
-        objective: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio eu feugiat pretium nibh ipsum. Dolor sit amet consectetur adipiscing. Vel fringilla est ullamcorper eget nulla facilisi. Orci phasellus egestas tellus rutrum tellus. Morbi non arcu risus quis varius. Vitae nunc sed velit dignissim. Fusce ut placerat orci nulla pellentesque. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros in.`
+        objective: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio eu feugiat pretium nibh ipsum. Dolor sit amet consectetur adipiscing. Vel fringilla est ullamcorper eget nulla facilisi. Orci phasellus egestas tellus rutrum tellus. Morbi non arcu risus quis varius. Vitae nunc sed velit dignissim. Fusce ut placerat orci nulla pellentesque. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros in.`,
+        collaborators: [
+            {
+                fullName: `Karangu Lucas Wairore`,
+                githubProfile: `https://github.com/lwairore`,
+                githubUsername: `lwairore`,
+                avatarURL: `/assets/img/me.png`
+            }
+        ]
     },
     {
         heading: `InstaClone`,
@@ -35,7 +43,15 @@ export const PROJECTS: Project[] = [
         createdAt: new Date(`Aug 30, 2020`),
         projectGallery: `/assets/img/portfolio/single/slider/02.jpg`,
         githubRepo: `https://github.com/{:owner}/{:repoName}`,
-        objective: `Lectus sit amet est placerat in. At quis risus sed vulputate odio ut enim blandit. Pulvinar pellentesque habitant morbi tristique senectus et. Erat imperdiet sed euismod nisi porta lorem mollis aliquam ut. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Imperdiet nulla malesuada pellentesque elit eget gravida cum. Urna id volutpat lacus laoreet non curabitur gravida arcu. Quis enim lobortis scelerisque fermentum dui.`
+        objective: `Lectus sit amet est placerat in. At quis risus sed vulputate odio ut enim blandit. Pulvinar pellentesque habitant morbi tristique senectus et. Erat imperdiet sed euismod nisi porta lorem mollis aliquam ut. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames. Imperdiet nulla malesuada pellentesque elit eget gravida cum. Urna id volutpat lacus laoreet non curabitur gravida arcu. Quis enim lobortis scelerisque fermentum dui.`,
+        collaborators: [
+            {
+                fullName: `Karangu Lucas Wairore`,
+                githubProfile: `https://github.com/lwairore`,
+                githubUsername: `lwairore`,
+                avatarURL: `/assets/img/me.png`
+            }
+        ]
     },
     {
         heading: `News-Highlights`,
@@ -53,7 +69,15 @@ export const PROJECTS: Project[] = [
         createdAt: new Date(`Sept 3, 2020`),
         projectGallery: `/assets/img/portfolio/single/slider/03.jpg`,
         githubRepo: `https://github.com/{:owner}/{:repoName}`,
-        objective: `Sed enim ut sem viverra. Nisi vitae suscipit tellus mauris. Scelerisque eleifend donec pretium vulputate. Massa id neque aliquam vestibulum morbi blandit cursus risus. Lorem ipsum dolor sit amet consectetur. Netus et malesuada fames ac turpis egestas maecenas pharetra convallis. Commodo nulla facilisi nullam vehicula ipsum a. Venenatis cras sed felis eget velit aliquet sagittis. Tincidunt augue interdum velit euismod in pellentesque massa placerat. Quis vel eros donec ac odio tempor orci dapibus. Elementum tempus egestas sed sed risus. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Tincidunt id aliquet risus feugiat in ante. Euismod in pellentesque massa placerat duis ultricies. Purus in mollis nunc sed. Ultricies tristique nulla aliquet enim. Congue quisque egestas diam in.`
+        objective: `Sed enim ut sem viverra. Nisi vitae suscipit tellus mauris. Scelerisque eleifend donec pretium vulputate. Massa id neque aliquam vestibulum morbi blandit cursus risus. Lorem ipsum dolor sit amet consectetur. Netus et malesuada fames ac turpis egestas maecenas pharetra convallis. Commodo nulla facilisi nullam vehicula ipsum a. Venenatis cras sed felis eget velit aliquet sagittis. Tincidunt augue interdum velit euismod in pellentesque massa placerat. Quis vel eros donec ac odio tempor orci dapibus. Elementum tempus egestas sed sed risus. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Tincidunt id aliquet risus feugiat in ante. Euismod in pellentesque massa placerat duis ultricies. Purus in mollis nunc sed. Ultricies tristique nulla aliquet enim. Congue quisque egestas diam in.`,
+        collaborators: [
+            {
+                fullName: `Karangu Lucas Wairore`,
+                githubProfile: `https://github.com/lwairore`,
+                githubUsername: `lwairore`,
+                avatarURL: `/assets/img/me.png`
+            }
+        ]
     },
     {
         heading: `Pig-Dice`,
@@ -88,7 +112,15 @@ export const PROJECTS: Project[] = [
         createdAt: new Date(`Sept 6, 2020`),
         projectGallery: `/assets/img/portfolio/single/slider/04.jpg`,
         githubRepo: `https://github.com/{:owner}/{:repoName}`,
-        objective: `Cras sed felis eget velit aliquet sagittis id consectetur purus. Orci eu lobortis elementum nibh tellus molestie nunc. Non quam lacus suspendisse faucibus interdum. Urna cursus eget nunc scelerisque. Tortor id aliquet lectus proin nibh nisl. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. Quisque id diam vel quam elementum pulvinar etiam. Sed faucibus turpis in eu mi bibendum neque. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Risus nec feugiat in fermentum. Dis parturient montes nascetur ridiculus. Volutpat lacus laoreet non curabitur gravida arcu ac. Mattis aliquam faucibus purus in massa tempor nec feugiat. Ullamcorper dignissim cras tincidunt lobortis feugiat.`
+        objective: `Cras sed felis eget velit aliquet sagittis id consectetur purus. Orci eu lobortis elementum nibh tellus molestie nunc. Non quam lacus suspendisse faucibus interdum. Urna cursus eget nunc scelerisque. Tortor id aliquet lectus proin nibh nisl. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. Quisque id diam vel quam elementum pulvinar etiam. Sed faucibus turpis in eu mi bibendum neque. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Risus nec feugiat in fermentum. Dis parturient montes nascetur ridiculus. Volutpat lacus laoreet non curabitur gravida arcu ac. Mattis aliquam faucibus purus in massa tempor nec feugiat. Ullamcorper dignissim cras tincidunt lobortis feugiat.`,
+        collaborators: [
+            {
+                fullName: `Karangu Lucas Wairore`,
+                githubProfile: `https://github.com/lwairore`,
+                githubUsername: `lwairore`,
+                avatarURL: `/assets/img/me.png`
+            }
+        ]
     },
     {
         heading: `Quotes`,
@@ -106,7 +138,15 @@ export const PROJECTS: Project[] = [
         createdAt: new Date(`Sept 9, 2020`),
         projectGallery: `/assets/img/portfolio/single/slider/05.jpg`,
         githubRepo: `https://github.com/{:owner}/{:repoName}`,
-        objective: `Varius vel pharetra vel turpis nunc. Vitae turpis massa sed elementum tempus egestas. Mauris cursus mattis molestie a. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Enim sed faucibus turpis in eu. Mi bibendum neque egestas congue quisque egestas diam in. Sed euismod nisi porta lorem mollis aliquam ut. Nunc faucibus a pellentesque sit amet. Sit amet consectetur adipiscing elit ut aliquam purus. Risus in hendrerit gravida rutrum. Arcu non sodales neque sodales ut. Nibh venenatis cras sed felis eget velit. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Purus viverra accumsan in nisl nisi scelerisque eu.`
+        objective: `Varius vel pharetra vel turpis nunc. Vitae turpis massa sed elementum tempus egestas. Mauris cursus mattis molestie a. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Enim sed faucibus turpis in eu. Mi bibendum neque egestas congue quisque egestas diam in. Sed euismod nisi porta lorem mollis aliquam ut. Nunc faucibus a pellentesque sit amet. Sit amet consectetur adipiscing elit ut aliquam purus. Risus in hendrerit gravida rutrum. Arcu non sodales neque sodales ut. Nibh venenatis cras sed felis eget velit. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Purus viverra accumsan in nisl nisi scelerisque eu.`,
+        collaborators: [
+            {
+                fullName: `Karangu Lucas Wairore`,
+                githubProfile: `https://github.com/lwairore`,
+                githubUsername: `lwairore`,
+                avatarURL: `/assets/img/me.png`
+            }
+        ]
     },
     {
         heading: `Quiz-Board`,
@@ -141,6 +181,14 @@ export const PROJECTS: Project[] = [
         createdAt: new Date(`Sept 13, 2020`),
         projectGallery: `/assets/img/portfolio/single/slider/01.jpg`,
         githubRepo: `https://github.com/{:owner}/{:repoName}`,
-        objective: `Egestas dui id ornare arcu odio ut. Cursus metus aliquam eleifend mi. Eget gravida cum sociis natoque penatibus et. Nec ullamcorper sit amet risus nullam eget felis eget. Justo nec ultrices dui sapien eget mi. Ut pharetra sit amet aliquam id diam. Et malesuada fames ac turpis egestas integer. Neque convallis a cras semper auctor neque vitae tempus quam. Bibendum ut tristique et egestas quis. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Non arcu risus quis varius quam. Leo urna molestie at elementum eu. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Arcu bibendum at varius vel pharetra vel. Donec et odio pellentesque diam volutpat commodo sed. Id venenatis a condimentum vitae sapien pellentesque habitant. Varius morbi enim nunc faucibus a pellentesque sit amet. Mi sit amet mauris commodo quis imperdiet massa tincidunt.`
+        objective: `Egestas dui id ornare arcu odio ut. Cursus metus aliquam eleifend mi. Eget gravida cum sociis natoque penatibus et. Nec ullamcorper sit amet risus nullam eget felis eget. Justo nec ultrices dui sapien eget mi. Ut pharetra sit amet aliquam id diam. Et malesuada fames ac turpis egestas integer. Neque convallis a cras semper auctor neque vitae tempus quam. Bibendum ut tristique et egestas quis. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Non arcu risus quis varius quam. Leo urna molestie at elementum eu. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Arcu bibendum at varius vel pharetra vel. Donec et odio pellentesque diam volutpat commodo sed. Id venenatis a condimentum vitae sapien pellentesque habitant. Varius morbi enim nunc faucibus a pellentesque sit amet. Mi sit amet mauris commodo quis imperdiet massa tincidunt.`,
+        collaborators: [
+            {
+                fullName: `Karangu Lucas Wairore`,
+                githubProfile: `https://github.com/lwairore`,
+                githubUsername: `lwairore`,
+                avatarURL: `/assets/img/me.png`
+            }
+        ]
     }
 ]
