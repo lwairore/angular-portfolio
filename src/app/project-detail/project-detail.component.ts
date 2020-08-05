c734bc0e90904c244bd338eaf32ba2bca6c94945
lwairore <kwairore@gmail.com>
2020-08-05 19:30:11 +0300

    - Add and call loadProjectRouteData() method in ngOnInit() method inorder to fetch project data in route.

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Project } from '../shared/project';
 
 @Component({
   selector: 'lwkp-project-detail',
@@ -7,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ProjectDetailComponent implements OnInit {
+  routeDataSubscription: Subscription;
+  projectDetails: Project;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadProjectRouteData();
+  }
+
+  loadProjectRouteData(): void {
+    this.routeDataSubscription = this.activatedRoute.data
+      .subscribe((data: { project: Project }) => {
+        this.projectDetails = data.project;
+      })
   }
 
 }
