21918eef4287a6876e2077fcb61122f6dc941c25
lwairore <kwairore@gmail.com>
2020-08-05 19:17:34 +0300

    - Add resolve property on ProjectDetailComponent that uses ProjectResolverService.

--- src/app/project-detail/project-detail-routing.module.ts
+++ src/app/project-detail/project-detail-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectDetailComponent } from './project-detail.component';
+import { ProjectResolverService } from './project-resolver.service';
 
 const routes: Routes = [
   {
     path: ':projectSlug',
-    component: ProjectDetailComponent
+    component: ProjectDetailComponent,
+    resolve: {
+      project: ProjectResolverService
+    }
   }
 ]
 
