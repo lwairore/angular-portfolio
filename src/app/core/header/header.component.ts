196d1489e2db40f57c07c191c94c79f82ecd6e1d
lwairore <kwairore@gmail.com>
2020-08-06 01:21:47 +0300

    - Use the ViewportScroller to always scroll to 0, 0 position when URL starts with '/project'. - Use the setHistoryScrollRestoration method of ViewportScroller to automatically disable the scroll restoration provided by the browser.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import { Location, ViewportScroller } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router'
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'lwkp-header',
@@ -7,8 +9,23 @@ import { Location } from '@angular/common';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public showBackButton: boolean = true;
 
-  constructor(private location: Location) { }
+  constructor(
+    private location: Location,
+    private router: Router,
+    private viewportScroller: ViewportScroller) {
+      this.router.events.pipe(
+        filter(e => e instanceof NavigationEnd))
+        .subscribe((e: NavigationEnd) => {
+          if (e.url.startsWith('/project')){
+            this.showBackButton = true;
+            this.viewportScroller.scrollToPosition([0, 0]);
+            
+          }
+          this.viewportScroller.setHistoryScrollRestoration("auto");
+        })
+     }
 
   ngOnInit(): void {
   }
