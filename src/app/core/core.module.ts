05c1b9371c5b377e0ad774655caa31b3b201efd0
lwairore <kwairore@gmail.com>
2020-08-05 12:51:37 +0300

    - Remove in-memory-web-service-api package.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HeaderComponent, ContactMeComponent],
   imports: [
     CommonModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    ) 
   ],
   exports: [
     HeaderComponent,
