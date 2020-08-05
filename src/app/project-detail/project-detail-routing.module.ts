6f8f8be79056c0bb7269c0efa9999c62e3834547
lwairore <kwairore@gmail.com>
2020-08-05 11:15:05 +0300

    - Generate Project interface.

--- src/app/project-detail/project-detail-routing.module.ts
+++ src/app/project-detail/project-detail-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ProjectDetailComponent } from './project-detail.component';
 
-
+const routes: Routes = [
+  {
+    path: ':projectSlug',
+    component: ProjectDetailComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class ProjectDetailRoutingModule { }
