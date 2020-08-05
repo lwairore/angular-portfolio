9b8d31ddbddfee16399b5129f401f2333ec2fbf6
lwairore <kwairore@gmail.com>
2020-08-05 21:14:44 +0300

    - Generate ProjectGalleryComponent.

---
+++ src/app/project-detail/project-gallery/project-gallery.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-project-gallery',
+  templateUrl: './project-gallery.component.html',
+  styles: [
+  ]
+})
+export class ProjectGalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
