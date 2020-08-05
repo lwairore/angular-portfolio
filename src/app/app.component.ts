6c23332ea737e4a332b40b1164976509770350bd
lwairore <kwairore@gmail.com>
2020-08-05 15:19:14 +0300

    - Add trackNavigationLoading() method that tracks the progress of navigation setting the value of loading to true when navigation has ended, and setting the value of false when navigation has begun.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Event, NavigationCancel, NavigationEnd, NavigationError, Router, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'lwkp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnDestroy, OnInit {
+  
+  routerSubscription: Subscription;
+  loading: boolean;
+  
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.trackNavigationLoading();
+  }
+
+  trackNavigationLoading(): void {
+    this.routerSubscription = this.router.events.subscribe((event: Event) => {
+      switch(true){
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription.unsubscribe();
+  }
+  
 }
