806e729eb8e7cd101d38605b1c43214b02ad5d39
lwairore <kwairore@gmail.com>
2020-08-05 20:50:25 +0300

    - Declare PropertyGalleryComponent.

--- src/app/project-detail/project-detail.module.ts
+++ src/app/project-detail/project-detail.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ProjectDetailRoutingModule } from './project-detail-routing.module';
 import { ProjectDetailComponent } from './project-detail.component';
 import { ProjectTitleComponent } from './project-title/project-title.component';
+import { PropertyGalleryComponent } from './property-gallery/property-gallery.component';
 
 
 
 @NgModule({
-  declarations: [ProjectDetailComponent, ProjectTitleComponent],
+  declarations: [ProjectDetailComponent, ProjectTitleComponent, PropertyGalleryComponent],
   imports: [
     CommonModule,
     ProjectDetailRoutingModule
