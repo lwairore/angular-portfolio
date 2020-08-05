303a1253b3344b497cc6efc9b92bfe005b753021
lwairore <kwairore@gmail.com>
2020-08-05 14:27:40 +0300

    - Retrieve the respective projects for a particular framework.

--- src/app/homepage/portfolio-cards/portfolio-cards.component.ts
+++ src/app/homepage/portfolio-cards/portfolio-cards.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProjectService } from '../../core/project.service';
+import { Project } from 'src/app/core/project';
 
 @Component({
   selector: 'lwkp-portfolio-cards',
@@ -6,11 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PortfolioCardsComponent implements OnInit {
+export class PortfolioCardsComponent implements OnInit, OnDestroy {
+  routeSubscription: Subscription;
+  projectServiceSubscription: Subscription;
+  projects: Project[];
+  frameworkSlug: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.routeSubscription = this.route.params.subscribe((params) => {
+      this.frameworkSlug = params['frameworkSlug']
+      this.projectServiceSubscription = this.projectService.getSpecificProjectsForAFramework(this.frameworkSlug).subscribe(
+        (projects: Project[]) => {
+          this.projects = projects;
+        }
+      )
+      if (this.projects.length < 1){
+        if (this.frameworkSlug === '/' || this.frameworkSlug === 'all' || this.frameworkSlug === undefined){
+          this.projectServiceSubscription = this.projectService.getAllProjects().subscribe(
+            (projects: Project[]) => {
+              this.projects = projects;
+            }
+          )
+        }
+  
+        else {
+          this.router.navigateByUrl(`/all`);
+        }
+      }
+    }, (err) => console.log(err))
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
+    this.projectServiceSubscription.unsubscribe();
   }
 
 }
