d47e29d3497bb5058bf051ac9eaf9f3a10d277cb
lwairore <kwairore@gmail.com>
2020-08-05 15:23:52 +0300

    - Use HostListener and HostBinding decorators to dynamically set the class values.

--- src/app/shared/transform-navbar.directive.ts
+++ src/app/shared/transform-navbar.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[lwkpTransformNavbar]'
 })
 export class TransformNavbarDirective {
+  private toggleHomepageNavbar: boolean;
 
   constructor() { }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+    this.toggleHomepageNavbar = false;
+    if (window.pageYOffset > 500){
+      this.toggleHomepageNavbar = true;
+    }
+  }
+
+  private handleNavbarForHomepageComponent(): string {
+    if (this.toggleHomepageNavbar){
+      return `cs-header navbar navbar-expand-lg navbar-floating navbar-sticky navbar-light navbar-stuck`;
+    } else {
+      return `cs-header navbar navbar-expand-lg navbar-dark navbar-floating navbar-sticky`;
+    }
+  }
+
+  @HostBinding('class') get classes() {
+    return this.handleNavbarForHomepageComponent();
+  }
 
 }
