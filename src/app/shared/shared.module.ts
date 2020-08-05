86eb2c0b749c4369aae3334b50492bddad36bcdd
lwairore <kwairore@gmail.com>
2020-08-05 14:47:12 +0300

    - Declare TransformNavbarDirective.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TransformNavbarDirective } from './transform-navbar.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TransformNavbarDirective],
   imports: [
     CommonModule
   ]
