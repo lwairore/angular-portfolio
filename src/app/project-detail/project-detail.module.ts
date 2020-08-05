10c650dc977621d5c447604c701eb8370609170d
lwairore <kwairore@gmail.com>
2020-08-05 21:28:35 +0300

    - Declare ProjectDescriptionComponent.

--- src/app/project-detail/project-detail.module.ts
+++ src/app/project-detail/project-detail.module.ts
@@ -4,11 +4,12 @@ import { ProjectDetailRoutingModule } from './project-detail-routing.module';
 import { ProjectDetailComponent } from './project-detail.component';
 import { ProjectTitleComponent } from './project-title/project-title.component';
 import { ProjectGalleryComponent } from './project-gallery/project-gallery.component';
+import { ProjectDescriptionComponent } from './project-description/project-description.component';
 
 
 
 @NgModule({
-  declarations: [ProjectDetailComponent, ProjectTitleComponent, ProjectGalleryComponent],
+  declarations: [ProjectDetailComponent, ProjectTitleComponent, ProjectGalleryComponent, ProjectDescriptionComponent],
   imports: [
     CommonModule,
     ProjectDetailRoutingModule
