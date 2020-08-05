a0dda37ad12d2f55713dd2c0d1b60b6ad19db973
lwairore <kwairore@gmail.com>
2020-08-05 10:22:41 +0300

    - Declare ProjectDetailComponent.

--- src/app/project-detail/project-detail.module.ts
+++ src/app/project-detail/project-detail.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectDetailRoutingModule } from './project-detail-routing.module';
+import { ProjectDetailComponent } from './project-detail.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProjectDetailComponent],
   imports: [
     CommonModule,
     ProjectDetailRoutingModule
