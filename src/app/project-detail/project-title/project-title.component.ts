e91c51f235858eb6cdcf812d491d2e65494f1349
lwairore <kwairore@gmail.com>
2020-08-05 15:32:09 +0300

    - Generate ProjectTitleComponent.

---
+++ src/app/project-detail/project-title/project-title.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-project-title',
+  templateUrl: './project-title.component.html',
+  styles: [
+  ]
+})
+export class ProjectTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
