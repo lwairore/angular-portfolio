cc904f772c86a86d611e6284e6917d5853bfd82f
lwairore <kwairore@gmail.com>
2020-08-05 20:50:10 +0300

    - Generate PRopertyGalleryComponent.

---
+++ src/app/project-detail/property-gallery/property-gallery.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-property-gallery',
+  templateUrl: './property-gallery.component.html',
+  styles: [
+  ]
+})
+export class PropertyGalleryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
