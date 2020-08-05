f34279781cc0571a9008cfa66540aaa270a28cf7
lwairore <kwairore@gmail.com>
2020-08-05 10:22:25 +0300

    - Generate ProjectDetailComponent.

---
+++ src/app/project-detail/project-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lwkp-project-detail',
+  templateUrl: './project-detail.component.html',
+  styles: [
+  ]
+})
+export class ProjectDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
