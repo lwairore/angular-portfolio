523905ba2aecf829ed93c9b9cf814bce9df186e9
lwairore <kwairore@gmail.com>
2020-08-05 12:34:05 +0300

    - Enable HTTP services.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -5,13 +5,14 @@ import { PageTitleComponent } from './page-title/page-title.component';
 import { PortfolioGridComponent } from './portfolio-grid/portfolio-grid.component';
 import { PortfolioCardsComponent } from './portfolio-cards/portfolio-cards.component';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [HomepageComponent, PageTitleComponent, PortfolioGridComponent, PortfolioCardsComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule
   ]
 })
 export class HomepageModule { }
