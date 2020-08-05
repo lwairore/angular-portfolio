2b96854a014eb2d16aa9be391c29af15c3d02ece
lwairore <kwairore@gmail.com>
2020-08-05 15:23:31 +0300

    - Import SharedModule.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent, ContactMeComponent],
   imports: [
     CommonModule,
+    SharedModule
   ],
   exports: [
     HeaderComponent,
