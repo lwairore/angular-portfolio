5b879f714d8dc94ed419cd978001a09ad7509c10
lwairore <kwairore@gmail.com>
2020-08-10 23:30:16 +0300

    - Create and export TOOLS array constant.

---
+++ src/app/core/mock-tools.ts
@@ -0,0 +1,18 @@
+export const TOOLS: {title: string, slug: string}[] = [
+    {
+        title: `Angular`,
+        slug: `angular`
+    },
+    {
+        title: `Flask`,
+        slug: `flask`
+    },
+    {
+        title: `Django`,
+        slug: `django`
+    },
+    {
+        title: `Python`,
+        slug: `python`
+    }
+]
